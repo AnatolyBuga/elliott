895843f72f5627f70dcfcd6780d439e4589564cd
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2024-07-27 12:40:20 +0100

    added sesoning

--- pola/dataset.py
+++ pola/dataset.py
@@ -1,6 +1,8 @@
 from abc import ABC
+import datetime
 
 import pandas as pd
+import polars as pl
 
 
 # Extendable!
@@ -34,16 +36,48 @@ class PaymentDueTabInfo(LoanDataTabInfo):
 
 
 class DataSet:
-    def __init__(self, data: pd.DataFrame):
-        self.data = data
+    def __init__(self, data_df: pd.DataFrame, static_df: pd.DataFrame, key:str):
+        self.data_df   = data_df
+        self.static_df = static_df
+        self.key = key
+
+
+
+    def seasoning(self):
+        """ Computes Seasoning """
+        # We want to vectorise the computation, so use polars
+        pl_stat = pl.from_pandas(self.static_df)
+        res = pd.DataFrame(self.static_df[self.key])
+        for d in self.get_date_cols():
+            # For simplicity assuming 30 days per month
+            sasoning_per_month = (d - pl_stat['origination_date'] ).dt.total_days() / 30
+            sasoning_per_month_rounded = sasoning_per_month.round()
+            res[d] = sasoning_per_month_rounded.to_pandas()
+        res['Data'] = 'Seasoning' 
+        return res
+    
+    def add_seasoning(self):
+        """ Adds seasoning """
+        s = self.seasoning()
+        self.data_df = pd.concat((self.data_df, s), axis=0).sort_values(by="loan_id")
+        return self.data_df
+
+    def get_date_cols(self) -> list[datetime.date]:
+        return [col for col in self.data_df.columns if isinstance(col, datetime.date) ]
+    
+    def all_data(self):
+        """returns static and monthly Data as one"""
+        return pd.merge(self.data_df, self.static_df, how="outer", on=self.key)
 
     @classmethod
     def from_excel(
         cls,
         path: str = "2024 - Strat Casestudy.xlsx",
-        key="load_id",
-        static_tab: str = "",
+        key="loan_id",
+        static_tab: str = "", # TODO move this to StaticTabInfo
         static_tab_skip_rows: int = 2,
+        static_tab_skip_columns: int = 1,
+
         data_tabs: list[LoanDataTabInfo] = [],
     ):
         """reads tabs of a single excel file, concatenates into one"""
@@ -51,12 +85,16 @@ class DataSet:
         # 1) First deal with Data tabs
         data_dfs = []
         for data_tab in data_tabs:
-            df = pd.read_excel(path, sheet_name=data_tab.tab_name)
-            # df = all_sheets_df[data_tab.tab_name]
-            df.insert(1, "Data", data_tab.long_name)
+            df = pd.read_excel(path, sheet_name=data_tab.tab_name, index_col=None) 
+            # lower every column name to make sure it matches           
             df.columns = [
                 col.lower() if isinstance(col, str) else col for col in df.columns
             ]
+            # Date only, don't need time
+            df.columns = [
+                col.date() if isinstance(col, datetime.datetime) else col for col in df.columns
+            ]
+            df.insert(1, "Data", data_tab.long_name)
             data_dfs.append(df)
 
         # Just in case of discrepancies , lower all column names
@@ -67,9 +105,13 @@ class DataSet:
         data_df = data_df.sort_values(by="loan_id")
 
         # 2) Now join with Static
+        # Pandas limitation: first read all columns column names(just names)
+        all_columns = pd.read_excel(path, sheet_name=static_tab, skiprows=static_tab_skip_rows, nrows=0).columns
+        # the filter out unwanted columns
+        columns_to_use = all_columns[static_tab_skip_columns:]
+        # now only read columns and rows we want
         static_df = pd.read_excel(
-            path, sheet_name=static_tab, skiprows=static_tab_skip_rows
+            path, sheet_name=static_tab, skiprows=static_tab_skip_rows, usecols=columns_to_use
         )
-        data_static_df = pd.merge(data_df, static_df, how="outer", on=key)
-
-        return cls(data_static_df)
+        
+        return cls(data_df, static_df, key)
--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from pola import DataSet, MonthEndBalanceTabInfo, PaymentDueTabInfo, PaymentMadeTabInfo
 
 loans_data = DataSet.from_excel(
+    path = "2024 - Strat Casestudy.xlsx",
     static_tab="DATA-Static",
     data_tabs=[
         MonthEndBalanceTabInfo("DATA-Month End Balances"),
         PaymentDueTabInfo("DATA-Payment Due"),
         PaymentMadeTabInfo("DATA-Payment Made"),
     ],
-)  # noqa: F821
-print(loans_data.data.head(10))
+)
+print(loans_data.all_data().head(10))
+print(loans_data.add_seasoning().head(10))
 k = 5
